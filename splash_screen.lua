5cda799254856927357227b6a4010d244d188626
Dante Beltran <dante.beltran@ocsbstudent.ca>
2018-04-25 13:44:11 -0400

    added stuff
    This reverts commit d27fb963c8e3088866e74a813afdc09901c73a00.

--- splash_screen.lua
+++ splash_screen.lua
@@ -24,9 +24,9 @@ local scene = composer.newScene( sceneName )
 -----------------------------------------------------------------------------------------
  
 -- The local variables for this scene
-local lSeperate
-local eSeperate
-local gamesSeperate
+local lSeparate
+local eSeparate
+local gamesSeparate
 local hyphenSeperate
 local scrollXSpeed = 5
 local scrollXSpeed2 = -5
@@ -42,48 +42,48 @@ splSoundChannel = audio.play(splSound)
 -- LOCAL FUNCTIONS
 --------------------------------------------------------------------------------------------
 
--- The function that moves the L across the screen
+-- The function that moves the L across the screen and rotates the image
 local function moveE(event)
-    eSeperate.x = eSeperate.x + scrollXSpeed
-    eSeperate:rotate(6)
-
-    if (eSeperate.x == 300) then
+    eSeparate.x = eSeparate.x + scrollXSpeed
+    eSeparate:rotate(6)
+    -- makes the image stop at 300 x and sets the rotation to 0
+    if (eSeparate.x == 300) then
         scrollXSpeed = 0
-        eSeperate.rotation = 0
+        eSeparate.rotation = 0
     end
 end
 
--- The function that moves the E across the screen
+-- The function that moves the E across the screen and rotates the image
 local function moveL(event)
-    lSeperate.x = lSeperate.x + scrollXSpeed2
-    lSeperate:rotate(-5.7)
-
-    if (lSeperate.x == 680) then
+    lSeparate.x = lSeparate.x + scrollXSpeed2
+    lSeparate:rotate(-5.7)
+    -- makes the image stop at 680 x and sets the rotation of the image to 0
+    if (lSeparate.x == 680) then
         scrollXSpeed2 = 0
-        lSeperate.rotation = 0
+        lSeparate.rotation = 0
     end
 end
 
--- The function that moves the Games across the screen
+-- The function that moves the Games across the screen 
 local function moveGames(event)
-    gamesSeperate.y = gamesSeperate.y + scrollYSpeed2
-
-    if (gamesSeperate.y == 513) then
+    gamesSeparate.y = gamesSeparate.y + scrollYSpeed2
+    -- makes the image stop at 513 y
+    if (gamesSeparate.y == 513) then
         scrollYSpeed2 = 0
     end
 end
 
--- The function that moves the Hyphen across the screen
+-- The function that moves the Hyphen across the screen and slowly changes the transparency
 local function moveHyphen(event)
-    hyphenSeperate.y = hyphenSeperate.y + scrollYSpeed
-    hyphenSeperate.alpha = hyphenSeperate.alpha + 0.03
-
-    if (hyphenSeperate.y == 270) then
+    hyphenSeparate.y = hyphenSeparate.y + scrollYSpeed
+    hyphenSeparate.alpha = hyphenSeparate.alpha + 0.03
+    -- makes the image stop moving at 270 y
+    if (hyphenSeparate.y == 270) then
         scrollYSpeed = 0
     end
 
 end
-
+-- makes the screen transition from the splash screen to the main menu screen
 local function gotoMainMenu( )
     composer.gotoScene( "main_menu", {effect = "zoomOutInFadeRotate", time = 500})
 end
@@ -100,41 +100,41 @@ function scene:create( event )
     -- set the background to be black
     display.setDefault("background", 0, 0, 0)
 
+    -- Insert the E image
+    eSeparate = display.newImageRect("Images/E Separate.png", 250, 250)
+
+    -- set the initial x and y position of the E
+    eSeparate.x = 0
+    eSeparate.y = display.contentHeight/3
+
     -- Insert the L image
-    eSeperate = display.newImageRect("Images/E Seperate.png", 300, 300)
+    lSeparate = display.newImageRect("Images/L Separate.png", 250, 250)
 
-    -- set the initial x and y position of the terry
-    eSeperate.x = 0
-    eSeperate.y = display.contentHeight/3
+    -- set the initial x and y position of the L
+    lSeparate.x = 1000
+    lSeparate.y = display.contentHeight/3
 
-    -- Insert the terry image
-    lSeperate = display.newImageRect("Images/L Seperate.png", 300, 300)
+    -- Insert the Games image
+    gamesSeparate = display.newImageRect("Images/Games Separate.png", 400, 200)
 
-    -- set the initial x and y position of the terry
-    lSeperate.x = 1000
-    lSeperate.y = display.contentHeight/3
+    -- set the initial x and y position of the games
+    gamesSeparate.x = 500
+    gamesSeparate.y = display.contentHeight/3*3
 
-    -- Insert the terry image
-    gamesSeperate = display.newImageRect("Images/Games Seperate.png", 400, 200)
+    -- Insert the hyphen image
+    hyphenSeparate = display.newImageRect("Images/Hyphen Separate.png", 150, 100)
 
-    -- set the initial x and y position of the terry
-    gamesSeperate.x = 500
-    gamesSeperate.y = display.contentHeight/3*3
-
-    -- Insert the terry image
-    hyphenSeperate = display.newImageRect("Images/Hyphen Seperate.png", 150, 100)
-
-    -- set the initial x and y position of the terry
-    hyphenSeperate.x = 490
-    hyphenSeperate.y = 0
+    -- set the initial x and y position of the hyphen
+    hyphenSeparate.x = 490
+    hyphenSeparate.y = 0
     --set hyphen to be transparent
-    hyphenSeperate.alpha = 0
+    hyphenSeparate.alpha = 0
 
     -- Insert objects into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( lSeperate )
-    sceneGroup:insert( eSeperate )
-    sceneGroup:insert( gamesSeperate )
-    sceneGroup:insert( hyphenSeperate )
+    sceneGroup:insert( lSeparate )
+    sceneGroup:insert( eSeparate )
+    sceneGroup:insert( gamesSeparate )
+    sceneGroup:insert( hyphenSeparate )
 
 end -- function scene:create( event )
 
@@ -159,7 +159,7 @@ function scene:show( event )
 
     elseif ( phase == "did" ) then
 
-        -- Call the moveL,E,Games,Hyphen function as soon as we enter the frame.
+        -- Call the move L,E,Games,Hyphen function as soon as we enter the frame.
         Runtime:addEventListener("enterFrame", moveL)
         Runtime:addEventListener("enterFrame", moveE)
         Runtime:addEventListener("enterFrame", moveGames)
